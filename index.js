5ae8d98e268156c8cdfd0a4b3769c93558a9d6ba
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2024-01-05 14:42:38 -0500

    Add files via upload
    updating files to include npm init command as well as additional 'require' Chalk for colours

--- index.js
+++ index.js
@@ -1,81 +1,55 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const chalk = require('chalk');
 const { argv } = require('process');
 
-function init() {
-inquirer
-fs
+
 
 charsValidate = (input) => {
     if(input.length <= 3) {return true}
-
+    else {return false}
+    console.log("Please enter maximum of 3 characters");
 }
+    fs
+    chalk
+    inquirer
+
+
 .prompt([
     {
         name: 'text',
-        message: 'Please enter up to 3 characters of text:'
+        message: 'Please enter up to 3 characters of text:',
+        validate: charsValidate,    
     },  
     {
-        name: 'whatMotiv',
-        message: 'What was the motiv for the project?',
+        name: 'colour',
+        type: 'checkbox',
+        choices: [''],
+        message: 'What colour would you prefer?',
     },
     {
-        name: 'why',
-        message: 'Why did you create the project?',
+        name: 'shape',
+        type: 'checkbox',
+        choices: ['Square','circle', 'triangle'],
+        message: 'What shape would you prefer?',
     },
-    {
-        name: 'whatSolved',
-        message: 'What does this project solve?',
-    },
-    {
-        name: 'whatLearned',
-        message: 'What did you learn from this project?',
-    },
-    {
-        name: 'runInstructions',
-        message: 'How do you run the project?',
-    },
-    {
-        name: 'licence',
-        message: 'What kind of licence does the project have?',
-    },
+    
 ])
 
 .then(answers => { //assigns variables to each answer that is prompted
     
-    title = `App Title: ${answers.title}`;
-    whatMotiv = `Motivation for the project: ${answers.whatMotiv}`;
-    why = `The reason for developement: ${answers.why}`;
-    whatSolved = `What did the App resolve? ${answers.whatSolved}`;
-    whatLearned = `What was learned from the App development: ${answers.whatLearned}`;
-    runInstructions = `Instructions to run App: ${answers.runInstructions}`;
-    licence = `App Licence: ${answers.licence}`;
-    
+    text = answers.text;
+    colour = answers.colour;
+    shape = answers.shape;
+
     //below creates an array with all answers so it can be displayed in the new file
-    const questions = [title, whatMotiv, why, whatSolved, whatLearned, runInstructions, licence];
+    const questions = [text, colour, shape];
     //below uses JSON to turn the array from [2] into a string
     const jsonStringData = JSON.stringify(questions, null, 2);
-    const fileName = "newfile.txt";
-    console.log(fileName);
     
-    // TODO: Create a function to write README file
-    fs.writeFileSync('./newfile.txt', jsonStringData), (err) =>
+    //writes the svg file
+    fs.writeFileSync('./logo.svg', jsonStringData), (err) =>
     err ? console.error(err) : console.log('Success!') //logs any errors
     
     console.log('Answers:', answers);
 });
-
-// TODO: Create an array of questions for user input
-/* const questions = [title. whatMotiv, why, WhatSolved, WhatLearned, runInstructions, licence]; */
-
-
-function writeToFile(fileName, data) {
-    /* fs.writeFile('README.md', process.argv)[2], (err) =>
-    err ? console.error(err) : console.log("hello") */
-    
-}
-
-}
-
-// Function call to initialize app
-init();
