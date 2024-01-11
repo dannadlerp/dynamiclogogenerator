a692cab652db6b3030545e1af9735fad821a5124
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2024-01-10 21:43:38 -0500

    Add files via upload
    after extensive testing, found out due to (most likely) the node version, had to convert to index.mjs instead of .js. seems to have fixed the require importing bug.

---
+++ index.mjs
@@ -0,0 +1,59 @@
+//const inquirer = require('inquirer');
+import inquirer from "inquirer";
+//const fs = require('fs');
+import fs from "fs";
+//const chalk = require('chalk');
+import chalk from "chalk";
+//const { argv } = require('process');
+import { argv } from "process";
+
+
+function charsValidate(input) {
+    if(input.length <= 3) {return true}
+    else {return false}
+    console.log("Please enter maximum of 3 characters");
+}
+
+    fs
+    chalk
+    inquirer
+
+
+.prompt([
+    {
+        name: 'text',
+        message: 'Please enter up to 3 characters of text:',
+        validate: charsValidate,    
+    },  
+    {
+        name: 'colour',
+        type: 'checkbox',
+        choices: [''],
+        message: 'What colour would you prefer?',
+    },
+    {
+        name: 'shape',
+        type: 'checkbox',
+        choices: ['Square','circle', 'triangle'],
+        message: 'What shape would you prefer?',
+    },
+    
+])
+
+.then(answers => { //assigns variables to each answer that is prompted
+    
+    text = answers.text;
+    colour = answers.colour;
+    shape = answers.shape;
+
+    //below creates an array with all answers so it can be displayed in the new file
+    const questions = [text, colour, shape];
+    //below uses JSON to turn the array from [2] into a string
+    const jsonStringData = JSON.stringify(questions, null, 2);
+    
+    //writes the svg file
+    fs.writeFileSync('./logo.svg', jsonStringData), (err) =>
+    err ? console.error(err) : console.log('Success!') //logs any errors
+    
+    console.log('Answers:', answers);
+});
