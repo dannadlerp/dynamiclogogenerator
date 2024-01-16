8c272a19fe3e35e7eb823a0f17577ee536e67340
unknown <dan.nadlerp@gmail.com>
2024-01-16 14:51:30 -0500

    added more functionality to shapes' classes. still fixing bug where shapes' classes arent writing to shapecode

---
+++ shapes.mjs
@@ -0,0 +1,58 @@
+let shapeCode = "";
+
+//defining different shape classes
+class Triangle {
+    constructor() {
+        this.shapeColour = "black";
+    }
+    
+    setColor() {
+        this.shapeColour = shapeColour;   
+    }
+    
+    render() {
+        const coordinates = "150,18 244,182 56,182"; //coordinates of triangle
+        shapeCode = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">  //create code for generating shape with variables from input<polygon points="${coordinates}" fill="${shapeColour}" />
+        <text xmlns="http://www.w3.org/2000/svg" x="150" y="150" font-size="50" text-anchor="middle" fill="${this.txtColour}">${this.text}</text>
+        </svg>`;
+    return shapeCode;
+    }
+};
+
+
+class Square {
+    constructor() {
+        this.shapeColour = "black";
+    }
+    
+    setColor() {
+        this.shapeColour = shapeColour;
+    }
+    render() {
+        shapeCode = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">  //create code for generating shape with variables from input
+        <rect x="45" y="45" width="200" height="200" stroke="black" fill="${shapeColour}" stroke-width="5"/>
+        <text xmlns="http://www.w3.org/2000/svg" x="150" y="150" font-size="60" text-anchor="middle" fill="${this.txtColour}">${this.text}</text>
+        </svg>`
+        return shapeCode;
+    }
+};
+
+class Circle {
+    constructor() {
+        this.shapeColour = "black";
+    }
+    
+    setColor() {
+        this.shapeColour = shapeColour;   
+    }
+    
+    render() {
+        
+        shapeCode = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
+        <circle xmlns="http://www.w3.org/2000/svg" cx="150" cy="100" r="80" fill="${this.shapeColour}"/>
+        <text xmlns="http://www.w3.org/2000/svg" x="150" y="125" font-size="60" text-anchor="middle" fill="${this.txtColour}">${this.text}</text>
+        </svg>`;
+        return shapeCode;
+    }
+};
+export {Circle, Square, Triangle, shapeCode}
