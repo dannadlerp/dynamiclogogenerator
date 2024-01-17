0a1da3ca2c562db8dd4a4e3f38ec9d1cb9baceb2
Dan Nadler <142226474+dannadlerp@users.noreply.github.com>
2024-01-17 14:18:26 -0500

    Add files via upload
    added examples folder with generated logos, fixed shape classes to accept variables passed, had bug that was not passing shapeCode var into svg file created - fixed

--- index.mjs
+++ index.mjs
@@ -1,68 +1,13 @@
 import inquirer from "inquirer";
 import fs from "fs";
-//import chalk from "chalk";
 import { argv } from "process";
 import jest from "jest";
+import { Circle, Square, Triangle, shapeCode} from "./shapes.mjs";
 
-//defining different shape classes
-class Triangle {
-    constructor() {
-        this.shapeColour = "black";
-    }
-    
-    setColor(shapeColour) {
-        this.shapeColour = shapeColour;   
-    }
-    
-    render() {
-        const coordinates = "150,18 244,182 56,182"; //coordinates of triangle
-        const ShapeCode = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">  //create code for generating shape with variables from input<polygon points="${coordinates}" fill="${this.colour}" />
-        <text xmlns="http://www.w3.org/2000/svg" x="150" y="150" font-size="50" text-anchor="middle" fill="${textColour}">${text}</text>
-        </svg>`;
-    return ShapeCode;
-    }
-};
+const init = async () => {
 
-
-class Square {
-    constructor() {
-        this.shapeColour = "black";
-    }
-    
-    setColor(shapeColour) {
-        this.shapeColour = shapeColour;
-    }
-    render() {
-        const ShapeCode = `<svg width="300" height="200" version="1.1" xmlns="http://www.w3.org/2000/svg">  //create code for generating shape with variables from input
-        <rect x="45" y="45" width="200" height="200" stroke="black" fill="${shapeColour}" stroke-width="5"/>
-        <text xmlns="http://www.w3.org/2000/svg" x="150" y="150" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>
-        </svg>`
-        return ShapeCode;
-    }
-};
-
-class Circle {
-    constructor() {
-        this.shapeColour = "black";
-    }
-    
-    setColor(colshapeColour) {
-        this.shapeColour = shapeColour;   
-    }
-    
-    render() {
-        
-        const ShapeCode = `<svg xmlns="http://www.w3.org/2000/svg" version="1.1" width="300" height="200">
-        <circle xmlns="http://www.w3.org/2000/svg" cx="150" cy="100" r="80" fill="${shapeColour}"/>
-        <text xmlns="http://www.w3.org/2000/svg" x="150" y="125" font-size="60" text-anchor="middle" fill="${textColour}">${text}</text>
-        </svg>`;
-        return ShapeCode;
-    }
-};
-
-const init = () => {
-
-const fileName = logo.svg;
+const fileName = 'logo.svg';
+let newShape = "";
 function charsValidate(input) {
     if(input.length <= 3) {return true}
     else {return false}
@@ -70,10 +15,8 @@ function charsValidate(input) {
 }
 
     fs
-    chalk
     jest
-    inquirer
-
+await inquirer
 
 .prompt([
     {
@@ -82,9 +25,15 @@ function charsValidate(input) {
         validate: charsValidate,    
     },  
     {
+        name: 'txtColour',
+        type: 'input',
+        default: '#00ff00',
+        message: 'What colour is the text? (provide colour name or hexcode)',
+    },
+    {
         name: 'shape',
-        type: 'checkbox',
-        choices: ['Square','circle', 'triangle'],
+        type: 'list',
+        choices: ['square','circle', 'triangle'],
         message: 'What shape would you prefer?',
     },
     {
@@ -97,27 +46,34 @@ function charsValidate(input) {
 
 .then(answers => { //assigns variables to each answer that is prompted
     
+    const shape = answers.shape;
+    let newShape;
     const text = answers.text;
     const textColour = answers.txtColour;
-    const shape = answers.shape;
-    const colour = answers.shapeColour;
+    const shapeColour = answers.shapeColour;
 
-    //below creates an array with all answers so it can be displayed in the new file
-    const questions = [text, colour, shape];
-    //below uses JSON to turn the array from [2] into a string
-    const jsonStringData = JSON.stringify(ShapeCode, null, 2);
+    if (shape === "square") {newShape = new Square(text, shapeColour, textColour);
+    } else if (shape === "triangle") {newShape = new Triangle(text, shapeColour, textColour);
+    } else if (shape === "circle") {newShape = new Circle(text, shapeColour, textColour);}
+    //const questions = [text, colour, shape];
+    newShape.render()
+    const jsonStringData = JSON.stringify(shapeCode, null, 2);
+    //fs.writeFileSync(`./${fileName}`, jsonStringData); (err) =>  //must be 300x200px
+    fs.writeFileSync(`./${fileName}`, JSON.parse(jsonStringData), (err) => {  //writes svg file  
+            if (err) {
+                console.error(err);
+            } else {
+                console.log('Success!');
+                console.log(`Generated ${fileName}`);   
+            }
+        });
     
-    //writes the svg file
-    fs.writeFileSync(`./${fileName}`, jsonStringData), (err) =>  //must be 300x200px
-    err ? console.error(err) : console.log('Success!') //logs any errors
-    
+    console.log(`this is the variable:${JSON.parse(jsonStringData)}`);
     console.log('Answers:', answers);
     console.log(`Generated ${fileName}`);
-
-});
-
+})
 }
 
 export {init, Square, Circle, Triangle};
-init //runs the program
+init(); //runs the program
 
